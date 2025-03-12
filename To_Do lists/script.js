761afdb6e097cadf66f42c9a516c2ddfa4599bc7
muhammadquaid <muhammadquaid912>
2025-03-12 09:44:17 +0500

    add  new file by manipulation

--- To_Do lists/script.js
+++ To_Do lists/script.js
@@ -1,16 +1,18 @@
 const inputs =document.getElementById("inp");
 const text = document.querySelector(".text");
+
 function add(){
 if (inputs.value=="") {
     alert('pleace enter task!')
 } else {
-     let NewEle= document.createElement("ul");
-       NewEle.innerHTML=`${inputs.value} <i class="fa-solid fa-pen"></i> <i class="fa-solid fa-trash"></i>`;
+     const NewEle= document.createElement("ul");
+       NewEle.innerHTML=`${inputs.value} <i class="fasolid fa-pen"></i> <i class="fa-solid fa-trash"></i>`;
         text.appendChild(NewEle);
         inputs.value="";
-         NewEle.querySelector("i").addEventListener("click", remove);
+         NewEle.querySelector("i").removeEventListener("click", remove)
         function remove() {
-            btn.addEventListener("click")
+            NewEle.remove()
         }
     }
 }
+NewEle.querySelector("i").removeEventListener("click", remove)
